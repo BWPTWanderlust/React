44680368865d55a794bc78adb093898984c53b3a
Blair <abrgroup@gmail.com>
2020-01-30 14:53:24 -0500

    setup test data between app.js and landingpage.js

---
+++ wanderlustreactapp/src/Components/LandingPage.js
@@ -0,0 +1,24 @@
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+
+function LandingPage(props) {
+  console.log("Props inside LandingPage Component", props);
+
+  return (
+    <div className="expedition-container">
+      {props.experience.map(el => {
+        return (
+          <div>
+            <Link to={`/experiences/${el.id}`}>
+              <p>Location: {el.location}</p>
+            </Link>
+            <p>Trip Type: {el.triptype}</p>
+            <p>Duration: {el.duration}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
+export default LandingPage;
