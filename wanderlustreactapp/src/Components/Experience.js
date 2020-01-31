eae3c815d81cc8a7e871a583bcf41ea22a5082d8
codeasaglacier <remingtonshock@gmail.com>
2020-01-30 17:11:03 -0900

    set up AddExperience and started Experience

---
+++ wanderlustreactapp/src/Components/Experience.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react'
+import AddExperience from './AddExperience'
+
+export default function Experiences() {
+
+    const [ events, setEvents ] = useState([])
+
+    return (
+        <div>
+            <AddExperience setEvents = { setEvents } />
+        </div>
+    )
+
+}
