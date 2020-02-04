7de11d560d0b14f42888f61d2e9493cff8b4743d
Blair <abrgroup@gmail.com>
2020-02-03 22:24:50 -0500

    fixed search issue

--- wanderlustreactapp/src/Components/NewUser.js
+++ wanderlustreactapp/src/Components/NewUser.js
@@ -3,47 +3,78 @@ import { useForm } from "react-hook-form";
 import axios from "axios";
 
 const NewUser = () => {
-  return <div>Form page - Wanderlust</div>;
-};
-
   const { register, errors, handleSubmit } = useForm();
-  const [user, setUser] = useState({});
+  const [user, setUser] = useState({
+    username: "",
+    password: "",
+    name: "",
+    email: ""
+  });
 
-  const onSubmit = data => {
-    const user = data;
+  const handleChanges = e => {
+    setUser({ ...user, [e.target.name]: e.target.value });
+  };
+
+  const onSubmit = () => {
+    // e.preventDefault();
+    // console.log("User object", user);
     axios
-      .post("https://reqres.in/api/users/2", user)
-      .then(res => setUser(res.data))
+      .post("https://bewanderlust.herokuapp.com/api/auth/register/user", user)
+      .then(res => {
+        setUser(res.data);
+        console.log("res", res);
+      })
       .catch(err => console.log(err));
   };
 
   return (
-    <form onSubmit={submitForm}>
-      <label htmlFor="username">Password</label>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <label htmlFor="username">Username</label>
       <input
         id="username"
         type="text"
         name="username"
         onChange={handleChanges}
-        value={data.username}
+        value={user.username}
+      />
+      <label htmlFor="name">Name</label>
+      <input
+        id="name"
+        type="text"
+        name="name"
+        onChange={handleChanges}
+        value={user.name}
+      />
+      <label htmlFor="email">Email</label>
+      <input
+        id="email"
+        type="email"
+        name="email"
+        onChange={handleChanges}
+        value={user.email}
       />
       <label htmlFor="password">Password</label>
       <input
         id="password"
-        name="text"
+        name="password"
+        type="password"
         onChange={handleChanges}
-        value={data.password}
+        value={user.password}
       />
-      <label htmlFor="organizer">Organizer</label>
+      {/* <label htmlFor="organizer">Organizer</label>
       <input
         id="organizer"
-        name="checkbox"
+        name="organizer"
+        type="checkbox"
         onChange={handleChanges}
-        value={data.organizer}
-      />
-      <button type="submit">New User</button>
-      <button type="submit">Login</button>
+        value={user.organizer}
+      /> */}
+      <button type="submit" onClick={() => console.log("Submitted")}>
+        Register
+      </button>
+      {/* <button type="submit">Login</button> */}
     </form>
-  )
+  );
+};
 
 export default NewUser;
