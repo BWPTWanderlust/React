a8fa76184e95f50a016b123ca5c4f63e9546cfa5
Blair <abrgroup@gmail.com>
2020-02-05 14:58:56 -0500

    stylistic changes to landingpage / newuser and searchbar

--- wanderlustreactapp/src/Components/SearchBar.js
+++ wanderlustreactapp/src/Components/SearchBar.js
@@ -1,51 +1,45 @@
-import React, { useState, useEffect } from 'react'
-import axios from 'axios'
-import App from '../App'
-
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import App from "../App";
 
 const Search = props => {
+  const [search, setSearch] = useState("");
+  const [result, setResult] = useState([]);
 
-    const [ search, setSearch ] = useState('')
-    const [ result, setResult ] = useState([])
+  const changeHandler = e => {
+    setSearch(e.target.value);
+  };
 
-    const changeHandler = e => {
-        setSearch( e.target.value )
-    }
+  useEffect(() => {
+    axios
+      .get(`https://bewanderlust.herokuapp.com/api/exps${search}`)
+      .then(res => {
+        setResult(res.data.results);
+      })
+      .catch(err => {
+        console.error(err);
+      });
+  }, [search]);
 
-    useEffect( () => {
-        axios
-        .get( `https://bewanderlust.herokuapp.com/api/${ search }` )
-        .then( res => {
-            setResult( res.data.results )
-        })
-        .catch( err => {
-            console.error( err )
-        })
-    }, [ search ])
+  return (
+    <div>
+      <form>
+        <label>
+          Search Wanderlust: {""}
+          <input
+            onChange={changeHandler}
+            type="text"
+            name="search"
+            value={search}
+          />
+        </label>
+      </form>
 
-    return (
-        <div>
-            <form>
-                <label>
-                    Search Wanderlust: { '' }
-                    <input
-                        onChange = { changeHandler }
-                        type = 'text'
-                        name = 'search'
-                        value = { search }
-                    />
-                </label>
-            </form>
+      {result.map(res => {
+        return <App data={res} />;
+      })}
+    </div>
+  );
+};
 
-            { result.map( res => {
-                return <App
-                    data = {
-                        res
-                    }/>
-            })}
-
-        </div>
-    );
-}
-
-export default Search
+export default Search;
