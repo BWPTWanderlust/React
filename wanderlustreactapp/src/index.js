f1b0f142548bf4d494b36d7915109fd9938b8e39
dangolbeeker <scottb305@outlook.com>
2020-02-05 21:07:11 -0500

    redux dependencys

--- wanderlustreactapp/src/index.js
+++ wanderlustreactapp/src/index.js
@@ -4,10 +4,22 @@ import { BrowserRouter as Router } from "react-router-dom";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import App from "./App";
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(thunk, logger)),
+);
 
 ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
-  document.getElementById("root")
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
+  document.getElementById('root'),
 );
---
+++ wanderlustreactapp/src/reducers/Tours.js
@@ -0,0 +1,5 @@
+// import {
+//     FETCH_TOURS_START,
+//     FETCH_TOURS_SUCCESS,
+//     FETCH_TOURS_FAILURE,
+// }
